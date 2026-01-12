9b36e5cef4e1d91851bf1cf8db1f637c037b2db0
Aarya Patil <aaryapatil1996@gmail.com>
2026-01-12 15:03:23 +0100

    Added a pyproject.toml structure

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from setuptools import setup
 
-import setuptools
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="tapify",
-    version="0.1",
-    author="Aarya Patil",
-    author_email="patil@astro.utoronto.ca",
-    description="A python package for multitaper periodograms",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/aaryapatil/tapify",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Science/Research"
-    ],
-    packages=setuptools.find_packages(),
-    install_requires=["numpy", "scipy"],
-    extras_require={
-        "extras": ["astropy", "nfft"],
-        "tests": ["pytest"]}
-)
+setup()
